49e9cad726434c8c54f60ef21f985f2f39f2ca12
Yeongdon <80868508+ydonchoi@users.noreply.github.com>
2024-05-08 15:29:37 +0900

    Update 0001-two-sum.py
    compare one of the shortest runtime codes with my initial trial

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,5 +1,6 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # below is one case of the shortest runtime codes
         num_map = {}
 
         for i, v in enumerate(nums):
@@ -8,3 +9,18 @@ class Solution:
             num_map[v] = i
 
         return num_map
+
+        # my init trial (RT: 1800ms)
+    # def twoSum(self, nums: List[int], target: int) -> List[int]:
+    #     output = []
+    #     idx, t = 0, 0
+        
+    #     while len(nums) > 0:
+    #         idx_v = nums.pop(idx)
+    #         for i, v in enumerate(nums):
+    #             if idx_v + v == target:
+    #                 output.append(t+idx)
+    #                 output.append(t+i+1)
+    #         t += 1
+
+    #     return output
