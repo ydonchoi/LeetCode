64becaebe6a7bf437c7bcfce4a460c085540a315
Yeongdon <80868508+ydonchoi@users.noreply.github.com>
2024-05-17 23:21:00 +0900

    Time: 74 ms (12.89%), Space: 19.3 MB (5.78%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
@@ -1,9 +1,19 @@
 class Solution:
     def reverseVowels(self, s: str) -> str:
+        
+        # 모음 리스트 생성
         vowels = ['a','e','o','i','u','A','E','O','I','U']
+        
+        # 주어진 문자열에서 모음 위치(index)를 나타내는 리스트 생성
         chk = [i for i in range(len(s)) for v in vowels if v == s[i]]
+        
+        # 주어진 문자열을 문자 단위로 분할
         S = [c for c in s]
+        
+        # 대칭되는 위치에 있는 모음끼리 치환
         for k in range(len(chk)//2):
             S[chk[k]], S[chk[-(k+1)]] = S[chk[-(k+1)]], S[chk[k]]
+            
+        # 하나의 문자열로 결합하여 반환
         s = "".join(S)
         return s
