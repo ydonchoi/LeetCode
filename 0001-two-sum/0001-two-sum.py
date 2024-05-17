90106437097e4b64814784e70399359db7e0a24a
Yeongdon <80868508+ydonchoi@users.noreply.github.com>
2024-05-17 21:45:26 +0900

    Time: 52 ms (89.60%), Space: 17.8 MB (41.49%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,26 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # below is one case of the shortest runtime codes
-        num_map = {}
 
-        for i, v in enumerate(nums):
-            if target - v in num_map:
-                return [i, num_map[target-v]]
-            num_map[v] = i
-
-        return num_map
-
-        # my init trial (RT: 1800ms)
-    # def twoSum(self, nums: List[int], target: int) -> List[int]:
-    #     output = []
-    #     idx, t = 0, 0
+        numToIndex: dict = {}
         
-    #     while len(nums) > 0:
-    #         idx_v = nums.pop(idx)
-    #         for i, v in enumerate(nums):
-    #             if idx_v + v == target:
-    #                 output.append(t+idx)
-    #                 output.append(t+i+1)
-    #         t += 1
-
-    #     return output
+        for i, num in enumerate(nums):
+    
+            if target - num in numToIndex:
+            
+                return numToIndex[target-num], i
+          
+            numToIndex[num] = i
