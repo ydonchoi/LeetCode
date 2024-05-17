44fde41f28996317f3e7da7b0b53a7c268e2d9b0
Yeongdon <80868508+ydonchoi@users.noreply.github.com>
2024-05-17 22:56:44 +0900

    Time: 24 ms (97.80%), Space: 16.7 MB (27.11%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -1,12 +1,15 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
         
+        # i = (문자, 공란 상관없이) 주어진 텍스트의 마지막 인덱스
         i = len(s) - 1
         
+        # 제일 마지막에 위치한 문자가 등장할 때까지 (index - 1)
         while i >= 0 and s[i] == ' ':
-            i -= 1
+            i -= 1     
         lastIndex = i
-        
+
+        # 마지막 문자 index로부터 빈 칸(space)이 나타날 때까지 (index - 1)
         while i >= 0 and s[i] != ' ':
             i-= 1
         
