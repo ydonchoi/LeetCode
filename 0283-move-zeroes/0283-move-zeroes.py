8533d88a92c26fe00eba24ea58f687958f2ddac0
Yeongdon <80868508+ydonchoi@users.noreply.github.com>
2024-05-17 23:04:45 +0900

    Time: 122 ms (78.57%), Space: 18 MB (96.82%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,9 +3,17 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        
+        # 두 포인터 지정
         a, b = 0, 0
+        
+        # 숫자 배열에서 반복 탐색
         while a < len(nums):
+            
+            # 탐색 포인터가 0이 아닌 포인트를 발견하면
             if nums[a] != 0:
+                
+                # 0을 가진 포인터와 값 교체
                 tmp = nums[a]
                 nums[a] = nums[b]
                 nums[b] = tmp
