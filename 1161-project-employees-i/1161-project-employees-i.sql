63c97468120508ea47e17b153471bb125dfe07c0
최영돈(YeongDon, Choi) <80868508+ydonchoi@users.noreply.github.com>
2025-04-23 23:55:58 +0900

    Time: 519 ms (78.47%), Space: 0B (100%) - LeetHub

---
+++ 1161-project-employees-i/1161-project-employees-i.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select p.project_id, round(sum(experience_years)/count(experience_years), 2) average_years from project p left outer join employee e on p.employee_id=e.employee_id group by p.project_id
