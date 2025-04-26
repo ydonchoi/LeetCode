b5b304903b8914d3b01cd04a16aeb86f98313c64
ydonchoi <ydonchoi@gmail.com>
2025-04-26 11:00:35 +0900

    rewrite_query_statement_250426

---
+++ 584.find-customer-referee.sql
@@ -0,0 +1,15 @@
+--
+-- @lc app=leetcode id=584 lang=mysql
+--
+-- [584] Find Customer Referee
+--
+
+-- @lc code=start
+# Write your MySQL query statement below
+
+SELECT name
+FROM Customer
+WHERE referee_id <> 2 or referee_id IS NULL;
+
+-- @lc code=end
+
