b68ae164e6dbcd05f9165e4ff1638650c86491a2
최영돈(YeongDon, Choi) <80868508+ydonchoi@users.noreply.github.com>
2025-04-28 23:11:27 +0900

    Time: 2 ms (31.73%), Space: 17.7 MB (46.75%) - LeetHub

--- 3379-score-of-a-string/3379-score-of-a-string.py
+++ 3379-score-of-a-string/3379-score-of-a-string.py
@@ -1,6 +1,12 @@
 class Solution:
     def scoreOfString(self, s: str) -> int:
+        lst = []
         sum = 0
-        for idx in range(len(s)-1):
-            sum += abs(ord(s[idx])-ord(s[idx+1]))
+
+        for idx in range(len(s)):
+            lst.append(ord(s[idx]))
+        
+        for idx in range(len(lst)-1):
+            sum += lst[idx] - lst[idx+1] if lst[idx] > lst[idx+1] else lst[idx+1] - lst[idx]
+
         return sum
